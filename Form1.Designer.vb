a0b658144b9a8e1162a4c7c2939b3780b32887cf
TAShingler <tylershingler@rocketmail.com>
2023-07-05 00:56:13 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,295 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class mainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.targetLvlSNUpDown = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.soulsNeededTextBox = New System.Windows.Forms.TextBox()
+        Me.initialLvlSNUpDown = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.currentSoulsLAUpDown = New System.Windows.Forms.NumericUpDown()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.levelsAvailableTextBox = New System.Windows.Forms.TextBox()
+        Me.currentLvlLAUpDown = New System.Windows.Forms.NumericUpDown()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.targetLvlSNUpDown, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.initialLvlSNUpDown, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.currentSoulsLAUpDown, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.currentLvlLAUpDown, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.targetLvlSNUpDown)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.soulsNeededTextBox)
+        Me.GroupBox1.Controls.Add(Me.initialLvlSNUpDown)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 35)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(230, 161)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Calculate Souls Needed"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 129)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 19)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Souls Needed"
+        '
+        'targetLvlSNUpDown
+        '
+        Me.targetLvlSNUpDown.Location = New System.Drawing.Point(164, 73)
+        Me.targetLvlSNUpDown.Maximum = New Decimal(New Integer() {802, 0, 0, 0})
+        Me.targetLvlSNUpDown.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.targetLvlSNUpDown.Name = "targetLvlSNUpDown"
+        Me.targetLvlSNUpDown.Size = New System.Drawing.Size(60, 26)
+        Me.targetLvlSNUpDown.TabIndex = 4
+        Me.targetLvlSNUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.targetLvlSNUpDown.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 19)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Target Level"
+        '
+        'soulsNeededTextBox
+        '
+        Me.soulsNeededTextBox.BackColor = System.Drawing.SystemColors.Control
+        Me.soulsNeededTextBox.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.soulsNeededTextBox.Location = New System.Drawing.Point(107, 126)
+        Me.soulsNeededTextBox.Name = "soulsNeededTextBox"
+        Me.soulsNeededTextBox.ReadOnly = True
+        Me.soulsNeededTextBox.Size = New System.Drawing.Size(117, 26)
+        Me.soulsNeededTextBox.TabIndex = 2
+        Me.soulsNeededTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'initialLvlSNUpDown
+        '
+        Me.initialLvlSNUpDown.Location = New System.Drawing.Point(164, 41)
+        Me.initialLvlSNUpDown.Maximum = New Decimal(New Integer() {802, 0, 0, 0})
+        Me.initialLvlSNUpDown.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.initialLvlSNUpDown.Name = "initialLvlSNUpDown"
+        Me.initialLvlSNUpDown.Size = New System.Drawing.Size(60, 26)
+        Me.initialLvlSNUpDown.TabIndex = 1
+        Me.initialLvlSNUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.initialLvlSNUpDown.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 19)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Initial Level"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.currentSoulsLAUpDown)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.levelsAvailableTextBox)
+        Me.GroupBox2.Controls.Add(Me.currentLvlLAUpDown)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(252, 35)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(230, 161)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Calculate Level-Ups Available"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 129)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(73, 19)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Level-Ups"
+        '
+        'currentSoulsLAUpDown
+        '
+        Me.currentSoulsLAUpDown.Location = New System.Drawing.Point(107, 73)
+        Me.currentSoulsLAUpDown.Maximum = New Decimal(New Integer() {1215752192, 23, 0, 0})
+        Me.currentSoulsLAUpDown.Name = "currentSoulsLAUpDown"
+        Me.currentSoulsLAUpDown.Size = New System.Drawing.Size(117, 26)
+        Me.currentSoulsLAUpDown.TabIndex = 4
+        Me.currentSoulsLAUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 75)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(92, 19)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Current Souls"
+        '
+        'levelsAvailableTextBox
+        '
+        Me.levelsAvailableTextBox.Location = New System.Drawing.Point(107, 126)
+        Me.levelsAvailableTextBox.Name = "levelsAvailableTextBox"
+        Me.levelsAvailableTextBox.ReadOnly = True
+        Me.levelsAvailableTextBox.Size = New System.Drawing.Size(117, 26)
+        Me.levelsAvailableTextBox.TabIndex = 2
+        Me.levelsAvailableTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'currentLvlLAUpDown
+        '
+        Me.currentLvlLAUpDown.Location = New System.Drawing.Point(164, 41)
+        Me.currentLvlLAUpDown.Maximum = New Decimal(New Integer() {802, 0, 0, 0})
+        Me.currentLvlLAUpDown.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.currentLvlLAUpDown.Name = "currentLvlLAUpDown"
+        Me.currentLvlLAUpDown.Size = New System.Drawing.Size(60, 26)
+        Me.currentLvlLAUpDown.TabIndex = 1
+        Me.currentLvlLAUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.currentLvlLAUpDown.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 43)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(92, 19)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Current Level"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(494, 24)
+        Me.MenuStrip1.TabIndex = 7
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.SaveToolStripMenuItem, Me.OpenToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NewToolStripMenuItem.Text = "&New"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SaveToolStripMenuItem.Text = "&Save"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.OpenToolStripMenuItem.Text = "&Open"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(177, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "&Exit"
+        '
+        'mainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(494, 210)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "mainForm"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.targetLvlSNUpDown, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.initialLvlSNUpDown, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.currentSoulsLAUpDown, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.currentLvlLAUpDown, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents targetLvlSNUpDown As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents soulsNeededTextBox As TextBox
+    Friend WithEvents initialLvlSNUpDown As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents currentSoulsLAUpDown As NumericUpDown
+    Friend WithEvents Label5 As Label
+    Friend WithEvents levelsAvailableTextBox As TextBox
+    Friend WithEvents currentLvlLAUpDown As NumericUpDown
+    Friend WithEvents Label6 As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Form1.vb
@@ -0,0 +1,219 @@
+﻿Imports System.IO
+
+Public Class mainForm
+    'Amount of souls needed to get from Soul Level 1 to Soul Level 11; the formula that is being used does not accurately output souls needed for these levels
+    Public Shared ReadOnly a() As UInteger = {673, 689, 706, 723, 740, 757, 775, 793, 811, 829}
+
+    Sub Main()
+        'do something
+    End Sub
+
+    ''' <summary>
+    ''' Calculates the number of souls needed to go from current level to target level.
+    ''' </summary>
+    ''' <param name="sender">object that raised the event</param>
+    ''' <param name="e">EventArgs passed to method</param>
+    Private Sub SoulsNeeded_TextChanged(sender As Object, e As EventArgs) Handles initialLvlSNUpDown.TextChanged, targetLvlSNUpDown.TextChanged
+
+        'check that TextBoxes for initial level and target level do not contain empty strings or are not null; exit sub if either TextBox contains a null value or empty string
+        If String.IsNullOrEmpty(initialLvlSNUpDown.Text) OrElse
+            String.IsNullOrEmpty(targetLvlSNUpDown.Text) Then Exit Sub
+
+        'check that TextBoxes for initial level and target level contain numbers; exit sub if either TextBox contains non-numeric values
+        If Not IsNumeric(initialLvlSNUpDown.Text) OrElse
+            Not IsNumeric(targetLvlSNUpDown.Text) Then Exit Sub
+
+        Dim iLvl As ULong = CULng(initialLvlSNUpDown.Text)  'initial level value entered by user
+        Dim tLvl As ULong = CULng(targetLvlSNUpDown.Text)   'target level value entered by user
+        Dim y As Decimal = 0                                'variable used to calculate souls needed to level-up once
+        Dim soulsNeeded As ULong = 0                        'variable for calculated souls needed to level up from initial level to target level
+
+        If iLvl - 1 < 0 Then    'might change to '< 1'
+            Exit Sub
+        End If
+
+        'calculate souls needed to get from initial level to target level
+        Do While iLvl < tLvl
+
+            If iLvl < 11 Then
+                'if initial level is below 11, get value of souls needed for level-up from array
+                y += a(iLvl - 1)
+            Else
+                'calculate souls needed to level-up once based on initial level
+                y += (0.02 * ((iLvl + 1) ^ 3)) + (3.06 * ((iLvl + 1) ^ 2)) + (105.6 * (iLvl + 1)) - 895
+            End If
+
+            'increment initial level by one to continue calculations
+            iLvl += 1
+        Loop
+
+        'get absolute value for souls needed to level up
+        soulsNeeded = Math.Abs(y)
+
+        'set Text property for soulsNeededTextBox TextBox
+        soulsNeededTextBox.Text = soulsNeeded
+
+    End Sub
+
+    ''' <summary>
+    ''' Calculates total possible level-ups based on characters current level and current souls.
+    ''' </summary>
+    ''' <param name="sender">object that raised the event</param>
+    ''' <param name="e">EventArgs passed to method</param>
+    Private Sub LevelUps_TextChanged(sender As Object, e As EventArgs) Handles currentLvlLAUpDown.TextChanged, currentSoulsLAUpDown.TextChanged
+
+        'check that TextBoxes for current level and current souls do not contain empty strings or are not null; exit sub if either TextBox contains a null value or empty string
+        If String.IsNullOrEmpty(currentLvlLAUpDown.Text) OrElse
+            String.IsNullOrEmpty(currentSoulsLAUpDown.Text) Then Exit Sub
+
+        'check that TextBoxes for current level and current souls contain numbers; exit sub if either TextBox contains non-numeric values
+        If Not IsNumeric(currentLvlLAUpDown.Text) OrElse
+            Not IsNumeric(currentSoulsLAUpDown.Text) Then Exit Sub
+
+        Dim levelsAvailable As Integer = 0                      'variable for calculated total possible level-ups
+        Dim lvlIncrement As Integer                             'used in place of cLevel during calculation
+        Dim y As Decimal = 0                                    'variable to calculate souls needed to level-up once
+        Dim soulsNeeded As ULong = 0                            'variable for calculated possible level-ups
+        Dim cLevel As ULong = CULng(currentLvlLAUpDown.Text)    'current level value entered by user
+        Dim cSouls As ULong = CULng(currentSoulsLAUpDown.Text)  'current souls value entered by user
+
+        If cLevel - 1 < 0 Then  'might change to '< 1'
+            Exit Sub
+        End If
+
+        lvlIncrement = cLevel
+
+        'calculate total possible level-ups from current level using current souls amount
+        Do While soulsNeeded <= cSouls 'cSouls >= soulsNeeded
+
+            If lvlIncrement < 11 Then
+                'if current level is less than 11, get value of souls needed for level-up from array
+                y = a(lvlIncrement - 1)
+            Else
+                'calculate souls needed to level-up once based on initial level
+                y = (0.02 * ((lvlIncrement + 1) ^ 3)) + (3.06 * ((lvlIncrement + 1) ^ 2)) + (105.6 * (lvlIncrement + 1)) - 895
+            End If
+
+            soulsNeeded += Math.Abs(y)  'increment total souls needed by amount calculated
+
+            levelsAvailable += 1    'increment level-ups available by 1
+            lvlIncrement += 1       'increment current level by 1
+
+        Loop
+
+        'display results of calculation
+        If cLevel = 802 Then
+            levelsAvailableTextBox.Text = "Already at Max Level"
+        End If
+
+        If cLevel + levelsAvailable > 802 Then
+            levelsAvailableTextBox.Text = 802 - cLevel
+        Else
+            levelsAvailableTextBox.Text = levelsAvailable - 1
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Resets Text property values for TextBoxes on Form to initial values.
+    ''' </summary>
+    ''' <param name="sender">object that raised the event</param>
+    ''' <param name="e">EventArgs passed to method</param>
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+
+        'set Text property value for input TextBoxes on Form to initial values
+        initialLvlSNUpDown.Text = 1
+        targetLvlSNUpDown.Text = 1
+        currentLvlLAUpDown.Text = 1
+        currentSoulsLAUpDown.Text = 0
+
+    End Sub
+
+    ''' <summary>
+    ''' Saves values for initial level, target level, current level, current souls from corresponding TextBoxes on Form to a text file.
+    ''' </summary>
+    ''' <param name="sender">object that raised the event</param>
+    ''' <param name="e">EventArgs passed to method</param>
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+
+        Dim writer As StreamWriter                                  'StreamWriter object to write data to file
+        Dim result As DialogResult                                  'DialogResult result object for event raised from SaveFileDialog
+        Dim fileName, fileData As String                            'variables for the name of file to save data to and data to save to file, respectively
+
+        'data to write to file
+        fileData = initialLvlSNUpDown.Text & "," & targetLvlSNUpDown.Text & "," & currentLvlLAUpDown.Text & "," & currentSoulsLAUpDown.Text
+
+        'display SaveFileDialog window for user to choose where to save file and with what name, save DialogResult
+        Using fileChooser As New SaveFileDialog()
+
+            fileChooser.Filter = "TXT Files (*.txt*)|*.txt" 'file extensions for user to choose from
+            result = fileChooser.ShowDialog()               'DialogResult from SaveFileDialog window
+            fileName = fileChooser.FileName                 'file name entered by user
+
+        End Using
+
+        'write date to file if user did not click Cancel button on SaveFileDialog window
+        If result <> Windows.Forms.DialogResult.Cancel Then
+
+            writer = New StreamWriter(fileName) 'creates new file and opens stream to write data to file if file does not exist, overwrites file if file does exist
+
+            'write data to file
+            writer.Write(fileData)
+
+            writer.Close()  'close StreamWriter stream
+
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Opens text file that contains values for initial level, target level, current level, and current souls for corresponding TextBoxes on Form.
+    ''' </summary>
+    ''' <param name="sender">object that raised the event</param>
+    ''' <param name="e">EventArgs passed to method</param>
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+
+        Dim reader As StreamReader      'StreamReader object to read data from file
+        Dim result As DialogResult      'DialogResult object for event raised from OpenFileDialog
+        Dim fileName As String          'variable for the name of the file to open
+
+        'display OpenFileDialog window for user to choose which file to open
+        Using fileChooser As New OpenFileDialog()
+
+            result = fileChooser.ShowDialog()   'DialogResult from OpenFileDialog window
+            fileName = fileChooser.FileName     'file name entered by user
+
+        End Using
+
+        'read from file if user did not click Cancel button on OpenFileDialog window
+        If result <> Windows.Forms.DialogResult.Cancel Then
+
+            reader = New StreamReader(fileName) 'creates stream to read data from file
+
+            Dim fileData As String = reader.ReadLine()      'read data from file
+            Dim fields() As String = fileData.Split(","c)   'split data in variable by comma value separator
+
+            'set Text property values for TextBoxes on Form
+            initialLvlSNUpDown.Text = fields(0)
+            targetLvlSNUpDown.Text = fields(1)
+            currentLvlLAUpDown.Text = fields(2)
+            currentSoulsLAUpDown.Text = fields(3)
+
+            reader.Close()  'close StreamReader stream
+
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Method that closes the current Form.
+    ''' </summary>
+    ''' <param name="sender">object that raised the event</param>
+    ''' <param name="e">EventArgs passed to method</param>
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+
+        Me.Close()  'closes Form, which terminates application
+
+    End Sub
+
+End Class
